05df34bc8f2ffb551a7c029161e0e2dec6d48b66
JAYABALAJEE S K GitHub <tharunbalajee04@gmail.com>
2025-03-14 15:55:51 +0530

    PUT and DELETE Methods - JobRestController

--- spring-boot-rest/src/main/java/com/jb/spring_boot_rest/JobRestController.java
+++ spring-boot-rest/src/main/java/com/jb/spring_boot_rest/JobRestController.java
@@ -3,9 +3,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.jb.spring_boot_rest.model.JobPost;
@@ -18,26 +20,41 @@ public class JobRestController {
 @Autowired
     private JobService service;
 
-    @GetMapping("jobPosts")
+    @GetMapping("jobPost")
     //@ResponseBody
     public List<JobPost> getAlljobs()
     {
         return service.getAllJobs();
 
     }
-    @GetMapping("jobPosts/{postId}")
+    @GetMapping("jobPost/{postId}")
     //@ResponseBody
-    public JobPost getjobs(@PathVariable("postId") int postId)
+    public JobPost getJobs(@PathVariable("postId") int postId)
     {
         return service.getJob(postId);
 
     }
 
-    @PostMapping("jobPosts")
+    @PostMapping("jobPost")
     public JobPost addJob(@RequestBody JobPost jobPost)
     {
         service.addJob(jobPost);
         return service.getJob(jobPost.getPostId());
 
     }
+
+    @PutMapping("jobPost")
+    public JobPost updateJobs(@RequestBody JobPost jobPost)
+    {
+        service.updateJob(jobPost);
+        return service.getJob(jobPost.getPostId());
+    }
+
+    @DeleteMapping("jobPost/{postId}")
+    public String deleteJob(@PathVariable("postId") int postId)
+    {
+        service.deleteJob(postId);
+        return "Deleted";
+    }
+
 }
