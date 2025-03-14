f497a0229230ca6f63a781e3d452e18622db7cd2
JAYABALAJEE S K GitHub <tharunbalajee04@gmail.com>
2025-03-14 12:56:38 +0530

    Used React UI and Connected React and Spring

--- spring-boot-rest/src/main/java/com/jb/spring_boot_rest/JobRestController.java
+++ spring-boot-rest/src/main/java/com/jb/spring_boot_rest/JobRestController.java
@@ -1,18 +1,16 @@
 package com.jb.spring_boot_rest;
-
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.jb.spring_boot_rest.model.JobPost;
 import com.jb.spring_boot_rest.service.JobService;
 
 @Controller
 @RestController
+@CrossOrigin(origins="http://localhost:3000/")
 public class JobRestController {
 @Autowired
     private JobService service;
@@ -24,5 +22,4 @@ public class JobRestController {
         return service.getAllJobs();
 
     }
-
 }
