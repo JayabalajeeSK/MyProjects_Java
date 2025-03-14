b8804d22a56f3ee4005ac5ddc480cf973fb6f493
JAYABALAJEE S K GitHub <tharunbalajee04@gmail.com>
2025-03-14 15:27:25 +0530

    @RquestBody - Sending data with @PostMapping

--- spring-boot-rest/src/main/java/com/jb/spring_boot_rest/JobRestController.java
+++ spring-boot-rest/src/main/java/com/jb/spring_boot_rest/JobRestController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.jb.spring_boot_rest.model.JobPost;
 import com.jb.spring_boot_rest.service.JobService;
@@ -28,6 +30,14 @@ public class JobRestController {
     public JobPost getjobs(@PathVariable("postId") int postId)
     {
         return service.getJob(postId);
+
+    }
+
+    @PostMapping("jobPosts")
+    public JobPost addJob(@RequestBody JobPost jobPost)
+    {
+        service.addJob(jobPost);
+        return service.getJob(jobPost.getPostId());
 
     }
 }
