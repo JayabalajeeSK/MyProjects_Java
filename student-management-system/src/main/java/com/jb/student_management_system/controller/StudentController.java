61c1476d37db5d7afa67f2943e14826576a54351
JAYABALAJEE S K GitHub <tharunbalajee04@gmail.com>
2025-03-16 12:10:47 +0530

    Backend - List Service Feature - Student Service Interface, Student Service Implementaion Class, Student Controller

---
+++ student-management-system/src/main/java/com/jb/student_management_system/controller/StudentController.java
@@ -0,0 +1,27 @@
+package com.jb.student_management_system.controller;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.jb.student_management_system.dto.StudentDto;
+import com.jb.student_management_system.service.StudentService;
+import org.springframework.ui.Model;
+
+@Controller//become mvc controller - capable to handle Rest API
+@RequestMapping
+public class StudentController 
+{
+    private StudentService studentService; // using interface
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+    @GetMapping("/student")
+    public String listStudents(Model model)
+    {
+        List<StudentDto> students = studentService.getAllStudents();
+        model.addAttribute("students", students);
+        return "student";
+
+    }
+}
---
+++ student-management-system/src/main/java/com/jb/student_management_system/service/StudentService.java
@@ -0,0 +1,8 @@
+package com.jb.student_management_system.service;
+import java.util.List;
+import com.jb.student_management_system.dto.StudentDto;
+public interface StudentService 
+{
+    List<StudentDto> getAllStudents();
+
+}
