9a015481b0dddedaccab49517cff2d464a7fbb69
JAYABALAJEE S K GitHub <tharunbalajee04@gmail.com>
2025-03-17 14:57:06 +0530

    Deposit Amount - PUT - REST API

--- banking-app/src/main/java/com/jb/banking_app/controller/AccountController.java
+++ banking-app/src/main/java/com/jb/banking_app/controller/AccountController.java
@@ -1,5 +1,7 @@
 package com.jb.banking_app.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -36,6 +39,16 @@ public class AccountController {
     public ResponseEntity<AccountDto> getAccountbyId(@PathVariable("id") Long id)
     {
         AccountDto accountDto = accountService.getAccountById(id);
+        return ResponseEntity.ok(accountDto);
+    }
+
+//////////////////////////////////// 
+    // deposit amount by ID Rest API
+    @PutMapping("/api/accounts/{id}")
+    public ResponseEntity<AccountDto> deposit(@PathVariable("id") Long id, @RequestBody Map<String, Double> request)
+    {
+        Double amount = request.get("amount");
+        AccountDto accountDto = accountService.deposit(id, amount);
         return ResponseEntity.ok(accountDto);
     }
 }
--- banking-app/src/main/java/com/jb/banking_app/service/AccountService.java
+++ banking-app/src/main/java/com/jb/banking_app/service/AccountService.java
@@ -7,4 +7,6 @@ public interface AccountService {
 
     AccountDto getAccountById(Long id);
 
+    AccountDto deposit(Long id, double amount);
+
 }
