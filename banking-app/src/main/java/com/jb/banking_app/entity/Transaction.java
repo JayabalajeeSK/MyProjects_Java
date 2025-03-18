978e567ab7392adf8ea098a5b39d22e29d8861db
JAYABALAJEE S K GitHub <tharunbalajee04@gmail.com>
2025-03-18 16:26:17 +0530

    Created Transaction Repository - Solved Error

--- banking-app/src/main/java/com/jb/banking_app/entity/Transaction.java
+++ banking-app/src/main/java/com/jb/banking_app/entity/Transaction.java
@@ -1,5 +1,7 @@
 package com.jb.banking_app.entity;
 import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,6 +24,7 @@ public class Transaction {
     private Long accountId;
     private double amount;
     private String transactionType; //deposit, withdraw, transfer
+    @Column(name = "time_stamp")
     private LocalDateTime timeStamp;
     
     public Transaction(Long id, Long accountId, double amount, String transactionType, LocalDateTime timeStamp) {
--- banking-app/src/main/java/com/jb/banking_app/repository/TransactionRepository.java
+++ banking-app/src/main/java/com/jb/banking_app/repository/TransactionRepository.java
@@ -1,16 +1,11 @@
-// package com.jb.banking_app.repository;
+package com.jb.banking_app.repository;
 
-// import java.util.List;
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.stereotype.Repository;
-// import com.jb.banking_app.entity.Transaction;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.jb.banking_app.entity.Transaction;
 
-// @Repository
-// public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-    
-//     // // Ensure that `account` is the correct field in Transaction entity
-
-//     // List<Transaction> findByAccountIdOrderBytimestampDesc(Long accountId);
-
-// }
-//
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findByAccountIdOrderByTimeStampDesc(Long accountId);
+}
