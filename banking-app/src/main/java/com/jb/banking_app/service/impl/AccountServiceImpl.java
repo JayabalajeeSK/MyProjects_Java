85ed630f04032e12f746e78e77cc6d75fd92da63
JAYABALAJEE S K GitHub <tharunbalajee04@gmail.com>
2025-03-18 17:35:04 +0530

    Log Transaction for Withdraw operation - Withdraw amount http://localhost:8080/api/accounts/1/withdraw  and check transaction table

--- banking-app/src/main/java/com/jb/banking_app/service/impl/AccountServiceImpl.java
+++ banking-app/src/main/java/com/jb/banking_app/service/impl/AccountServiceImpl.java
@@ -24,61 +24,70 @@ public class AccountServiceImpl implements AccountService {
     private TransactionRepository transactionRepository;
 
     private static final String TRANSACTION_TYPE_DEPOSIT = "DEPOSIT";
-    @Autowired
-    public AccountServiceImpl(AccountRepository accountRepository, TransactionRepository transactionRepository) {
-        this.accountRepository = accountRepository; 
-        this.transactionRepository = transactionRepository;//
-    }
-
-    @Override
-    public List<AccountDto> getAllAccounts() {
-        List<Account> accounts = accountRepository.findAll();
-        return accounts.stream().map((account) -> AccountMapper.mapToAccountDto(account))
-                                                        .collect(Collectors.toList());
-    }
-
-    @Override
-    public AccountDto createAccount(AccountDto accountDto) {
-        Account account=AccountMapper.mapToAccount(accountDto);
-        Account savedAccount=accountRepository.save(account);
-        return AccountMapper.mapToAccountDto(savedAccount);
+    private static final String TRANSACTION_TYPE_WITHDRAW = "WITHDRAW";
         
-    }
-
-    @Override
-    public AccountDto getAccountById(Long id) {
-        Account account = accountRepository.findById(id).orElseThrow(() -> new AccountException("Account Does not exist"));
-        return AccountMapper.mapToAccountDto(account);
-    }
-    //deposit///////////////////////////////////////////////////////////////////////
-    @Override
-    public AccountDto deposit(Long id, double amount) {
-        Account account = accountRepository.findById(id).orElseThrow(() -> new AccountException("Account Does not exist"));
-        double total = account.getBalance() + amount; ////
-        account.setBalance(total);
-        Account savedAccount = accountRepository.save(account);
-
-        Transaction transaction = new Transaction();
-        transaction.setAccountId(account.getId());
-        transaction.setAmount(amount);
-        transaction.setTransactionType(TRANSACTION_TYPE_DEPOSIT);
-        transaction.setTimeStamp(LocalDateTime.now());
-        
-        transactionRepository.save(transaction);
-        return AccountMapper.mapToAccountDto(savedAccount);
-    }
-
-    @Override
-    public AccountDto withdraw(Long id, double amount) {
-        Account account = accountRepository.findById(id).orElseThrow(() -> new AccountException("Account Does not exist"));
-        if(account.getBalance()<amount)
-        {
-            throw new RuntimeException("Insufficient amount");
+        public AccountServiceImpl(AccountRepository accountRepository, TransactionRepository transactionRepository) {
+            this.accountRepository = accountRepository; 
+            this.transactionRepository = transactionRepository;//
         }
-        double total = account.getBalance() - amount; ////
-        account.setBalance(total);
-        Account savedAccount = accountRepository.save(account);
-        return AccountMapper.mapToAccountDto(savedAccount);
+    
+        @Override
+        public List<AccountDto> getAllAccounts() {
+            List<Account> accounts = accountRepository.findAll();
+            return accounts.stream().map((account) -> AccountMapper.mapToAccountDto(account))
+                                                            .collect(Collectors.toList());
+        }
+    
+        @Override
+        public AccountDto createAccount(AccountDto accountDto) {
+            Account account=AccountMapper.mapToAccount(accountDto);
+            Account savedAccount=accountRepository.save(account);
+            return AccountMapper.mapToAccountDto(savedAccount);
+            
+        }
+    
+        @Override
+        public AccountDto getAccountById(Long id) {
+            Account account = accountRepository.findById(id).orElseThrow(() -> new AccountException("Account Does not exist"));
+            return AccountMapper.mapToAccountDto(account);
+        }
+        //deposit///////////////////////////////////////////////////////////////////////
+        @Override
+        public AccountDto deposit(Long id, double amount) {
+            Account account = accountRepository.findById(id).orElseThrow(() -> new AccountException("Account Does not exist"));
+            double total = account.getBalance() + amount; ////
+            account.setBalance(total);
+            Account savedAccount = accountRepository.save(account);
+    
+            Transaction transaction = new Transaction();
+            transaction.setAccountId(account.getId());
+            transaction.setAmount(amount);
+            transaction.setTransactionType(TRANSACTION_TYPE_DEPOSIT);
+            transaction.setTimeStamp(LocalDateTime.now());
+            
+            transactionRepository.save(transaction);
+            return AccountMapper.mapToAccountDto(savedAccount);
+        }
+        //withdraw/////////////////////////////////////////////////////////////////////////
+        @Override
+        public AccountDto withdraw(Long id, double amount) {
+            Account account = accountRepository.findById(id).orElseThrow(() -> new AccountException("Account Does not exist"));
+            if(account.getBalance() < amount)
+            {
+                throw new RuntimeException("Insufficient amount");
+            }
+            double total = account.getBalance() - amount; ////
+            account.setBalance(total);
+            Account savedAccount = accountRepository.save(account);
+
+            Transaction transaction = new Transaction();
+            transaction.setAccountId(id);
+            transaction.setAmount(amount);
+            transaction.setTransactionType(TRANSACTION_TYPE_WITHDRAW);
+            transaction.setTimeStamp(LocalDateTime.now());
+
+            transactionRepository.save(transaction);
+            return AccountMapper.mapToAccountDto(savedAccount);
 
     }
 
