87ee7e0393b51c4d9889163d328d061542b29a5d
JAYABALAJEE S K GitHub <tharunbalajee04@gmail.com>
2025-03-19 17:49:36 +0530

    Setting up the Portfolio project

---
+++ my-portfolio-jb-springboot/src/main/java/com/jb/my_portfolio_jb_springboot/MyPortfolioJbSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.jb.my_portfolio_jb_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPortfolioJbSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPortfolioJbSpringbootApplication.class, args);
+	}
+
+}
---
+++ my-portfolio-jb-springboot/src/test/java/com/jb/my_portfolio_jb_springboot/MyPortfolioJbSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jb.my_portfolio_jb_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPortfolioJbSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
