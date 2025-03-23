e8be025c7a9b964e397efbd9c0cde883d4ab9e67
JAYABALAJEE S K GitHub <tharunbalajee04@gmail.com>
2025-03-23 18:57:43 +0530

    Database Authenctication JB[230325] - Gmail (to Fix)

--- todo-management/src/main/java/com/jb/todo_management/config/SpringSecurityConfig.java
+++ todo-management/src/main/java/com/jb/todo_management/config/SpringSecurityConfig.java
@@ -2,24 +2,37 @@ package com.jb.todo_management.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 // import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+// import org.springframework.security.core.userdetails.User;
+// import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+// import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableMethodSecurity
 public class SpringSecurityConfig {
+
+    UserDetailsService userDetailsService;
+    public SpringSecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
     @Bean
     public static PasswordEncoder passwordEncoder()
     {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
@@ -42,21 +55,21 @@ public class SpringSecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService()
-    {
-        UserDetails tharun = User.builder()
-                                 .username("tharun")
-                                 .password(passwordEncoder().encode("user"))
-                                 .roles("USER")
-                                 .build();
+    // @Bean
+    // public UserDetailsService userDetailsService()
+    // {
+    //     UserDetails tharun = User.builder()
+    //                              .username("tharun")
+    //                              .password(passwordEncoder().encode("user"))
+    //                              .roles("USER")
+    //                              .build();
                     
-       UserDetails admin = User.builder()
-                                .username("admin")
-                                .password(passwordEncoder().encode("admin"))
-                                .roles("ADMIN")
-                                .build();
+    //    UserDetails admin = User.builder()
+    //                             .username("admin")
+    //                             .password(passwordEncoder().encode("admin"))
+    //                             .roles("ADMIN")
+    //                             .build();
 
-        return new InMemoryUserDetailsManager(tharun, admin);
-    }
+    //     return new InMemoryUserDetailsManager(tharun, admin);
+    // }
 }
---
+++ todo-management/src/main/java/com/jb/todo_management/utils/PasswordEncoderImpl.java
@@ -0,0 +1,12 @@
+package com.jb.todo_management.utils;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class PasswordEncoderImpl {
+    public static void main(String[] args) {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        System.out.println(passwordEncoder.encode("user"));//$2a$10$vvH8hBLSxTPb79lNAIpDLuoohzXdCE/0FbfyWF7fJrevBrf6C94dm
+        System.out.println(passwordEncoder.encode("admin"));//$2a$10$.e9n7MN.fyt2d3OQBmsShuk1pp5kx7P/udwy.Ng5IASI7AW4HoBGm
+    }
+}
