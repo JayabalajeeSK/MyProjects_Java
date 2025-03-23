281eb27ebcbf9944b6a63f6d25fcd28c08cb7e35
JAYABALAJEE S K GitHub <tharunbalajee04@gmail.com>
2025-03-23 17:23:28 +0530

    Creating USer and Role Repository - JPA

---
+++ todo-management/src/main/java/com/jb/todo_management/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.jb.todo_management.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.jb.todo_management.entity.Role;
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long>{
+
+}
---
+++ todo-management/src/main/java/com/jb/todo_management/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.jb.todo_management.repository;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.jb.todo_management.entity.User;
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByEmail(String email);
+
+    Optional<User> findByUsernameOrEmail(String username, String email);
+
+}
