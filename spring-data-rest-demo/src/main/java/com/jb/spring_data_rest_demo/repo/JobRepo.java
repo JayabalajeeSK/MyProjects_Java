c800a11af6a495d7e76b389283c5a756f73c2b0e
JAYABALAJEE S K GitHub <tharunbalajee04@gmail.com>
2025-03-25 10:00:33 +0530

    Spring Data Rest - Controller and Repo – Spring data rest – no need of service - Instead of web use Rest repositories - HATEOAS

---
+++ spring-data-rest-demo/src/main/java/com/jb/spring_data_rest_demo/repo/JobRepo.java
@@ -0,0 +1,13 @@
+package com.jb.spring_data_rest_demo.repo;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jb.spring_data_rest_demo.model.JobPost;
+
+@Repository
+public interface JobRepo extends JpaRepository<JobPost, Integer>{
+
+    List<JobPost> findByPostProfileContainingOrPostDescContaining(String postProfile, String postDesc);
+}
