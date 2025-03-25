2ca6dfca8ed6c55acf0705aae71da399d0e8587d
JAYABALAJEE S K GitHub <tharunbalajee04@gmail.com>
2025-03-25 11:40:42 +0530

    Validating Inputs using Around Advice

---
+++ spring-boot-rest/src/main/java/com/jb/spring_boot_rest/aop/ValidationAspect.java
@@ -0,0 +1,25 @@
+package com.jb.spring_boot_rest.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+@Component
+@Aspect
+public class ValidationAspect {
+        private static final Logger LOGGER = LoggerFactory.getLogger(ValidationAspect.class);
+        @Around("execution(* com.jb.spring_boot_rest.service.impl.JobServiceImplementation.getJob(..)) && args(postId)")
+        public Object validateAndUpdate(ProceedingJoinPoint jp, int postId) throws Throwable
+        {
+            if(postId<0)
+            {
+                LOGGER.info("postId is negative, updating it");
+                postId = -postId;
+                LOGGER.info("New value:" + postId);
+            }
+            Object obj = jp.proceed(new Object[]{postId});
+            return obj;
+        }
+}
