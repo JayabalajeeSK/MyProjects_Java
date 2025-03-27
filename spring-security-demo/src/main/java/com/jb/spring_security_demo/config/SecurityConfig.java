29e42bd35a186475d6b5edc0e23cda15c94c3f4d
JAYABALAJEE S K GitHub <tharunbalajee04@gmail.com>
2025-03-27 13:34:40 +0530

    Register and login for JWT - Student

--- spring-security-demo/src/main/java/com/jb/spring_security_demo/config/SecurityConfig.java
+++ spring-security-demo/src/main/java/com/jb/spring_security_demo/config/SecurityConfig.java
@@ -2,9 +2,11 @@ package com.jb.spring_security_demo.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -38,11 +40,20 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
     {
         http.csrf(customizer -> customizer.disable());// crsf -> csrf.disable()
-        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());//request authentication
+        http.authorizeHttpRequests(request -> request.requestMatchers("register", "login")
+        .permitAll()
+        .anyRequest()
+        .authenticated());//request authentication
         //http.formLogin(Customizer.withDefaults()); //login form //dont need form login when it is stateless
         http.httpBasic(Customizer.withDefaults()); //for security
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); //statefull to stateless
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception
+    {
+        return config.getAuthenticationManager();
     }
 
     // @Bean
--- spring-security-demo/src/main/java/com/jb/spring_security_demo/controller/UserController.java
+++ spring-security-demo/src/main/java/com/jb/spring_security_demo/controller/UserController.java
@@ -1,4 +1,8 @@
 package com.jb.spring_security_demo.controller;
+import java.net.Authenticator;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,15 +14,34 @@ import com.jb.spring_security_demo.service.UserService;
 public class UserController {
 
     private UserService service;
+    private AuthenticationManager authenticationManager;
     
-    public UserController(UserService service) {
+    
+
+    public UserController(UserService service, AuthenticationManager authenticationManager) {
         this.service = service;
+        this.authenticationManager = authenticationManager;
     }
 
     @PostMapping("/register")
     public User register(@RequestBody User user)
     {
         return service.saveUser(user);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody User user)
+    {
+        Authentication authentication = authenticationManager
+                                        .authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+        if(authentication.isAuthenticated())
+        {
+            return "Success";
+        }
+        else
+        {
+            return "Login Failed";
+        }
     }
 
 }
